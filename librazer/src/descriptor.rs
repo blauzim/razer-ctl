ad3c6da8a02acc55abf9739aaaf4231c1a8a496d
blauzim <73983999+blauzim@users.noreply.github.com>
2025-07-27 10:10:46 +1000

    Update descriptor.rs

--- librazer/src/descriptor.rs
+++ librazer/src/descriptor.rs
@@ -65,6 +65,20 @@ pub const SUPPORTED: &[Descriptor] = &[
             "perf",
         ],
         init_cmds : &[0x0081,0x0086,0x0f90,0x0086,0x0f10,0x0087],
+    },
+    Descriptor {
+        model_number_prefix: "RZ09-0421N",
+        name: "Razer Blade 15” (2022)",
+        pid: 0x028a,
+        features: &[
+            "battery-care",
+            "fan",
+            "kbd-backlight",
+            "lid-logo",
+            "lights-always-on",
+            "perf",
+        ],
+        init_cmds : &[],
     }
 ];
 
